0c9e6a4b6b70353751c623c77c23ec87c8c6df41
orch20 <94862153+orch20@users.noreply.github.com>
2022-08-10 19:39:37 +0300

    rewrite from class to hooks

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,45 +1,94 @@
 // import PropTypes from 'prop-types'
-import React, { Component } from 'react'
+import React, { useState } from 'react';
 import { Section } from './Section/Section';
 import { FeedbackOptions } from '../components/FedbackButtons/FeedbackButtons';
 import { Statistics } from '../components/Statistics/Statistics';
 import { Container } from './Conteiner';
 
-export class App extends Component {
-  // static propTypes = {}
- state = {
-    good: 0,
-    neutral: 0,
-    bad: 0,
+export const App = () => {
+  const [good, setGood] = useState(0);
+  const [neutral, setNeutral] = useState(0);
+  const [bad, setBad] = useState(0);
+
+  const feedbackValues = { good, neutral, bad };
+
+  const handleClick = event => {
+    const name = event.target.textContent;
+
+    switch (name) {
+      case 'good':
+        setGood(good + 1);
+        break;
+      case 'neutral':
+        setNeutral(neutral + 1);
+        break;
+      case 'bad':
+        setBad(bad + 1);
+        break;
+      default:
+        alert('qwerty');
+    }
+    console.log('event', name);
   };
 
-  handleClick = (event) => { 
-    const name  = event.target.textContent;
-    this.setState((prevState) => ({
-      [name]: prevState[name] + 1,
-    }));
-    console.log('event', name);
-  }
+  const countTotalFeedback = () =>
+    Object.values(feedbackValues).reduce((acc, feedback) => acc + feedback);
 
-    
+  const countPositiveFeedbackPercentage = () =>
+    Math.round((good / countTotalFeedback()) * 100) || 0;
 
-  countTotalFeedback = () => Object.values(this.state).reduce((acc, feedback) => acc + feedback);
-
-  countPositiveFeedbackPercentage = () => Math.round(this.state.good / this.countTotalFeedback() * 100) || 0;
-  
-
-  render() {
-    return (
-      <>
-        <Container>
+  return (
+    <>
+      <Container>
         <Section title={'Please leave feedback'} />
-        <FeedbackOptions handleClick={this.handleClick} option={this.state} />
+        <FeedbackOptions handleClick={handleClick} option={feedbackValues} />
         <Section title={'Statistics'} />
-        <Statistics stats={this.state} total={this.countTotalFeedback()} percentage={this.countPositiveFeedbackPercentage()}/>
-        </Container>
-      </>
-    )
-  }
-}
+        <Statistics
+          stats={feedbackValues}
+          total={countTotalFeedback()}
+          percentage={countPositiveFeedbackPercentage()}
+        />
+      </Container>
+    </>
+  );
+};
 
+// export class App extends Component {
+//   // static propTypes = {}
+//   state = {
+//     good: 0,
+//     neutral: 0,
+//     bad: 0,
+//   };
 
+//   handleClick = event => {
+//     const name = event.target.textContent;
+//     this.setState(prevState => ({
+//       [name]: prevState[name] + 1,
+//     }));
+//     console.log('event', name);
+//   };
+
+//   countTotalFeedback = () =>
+//     Object.values(this.state).reduce((acc, feedback) => acc + feedback);
+
+//   countPositiveFeedbackPercentage = () =>
+//     Math.round((this.state.good / this.countTotalFeedback()) * 100) || 0;
+
+//   render() {
+//     return (
+//       <>
+//         <Container>
+//           <Section title={'Please leave feedback'} />
+//           <FeedbackOptions handleClick={this.handleClick} option={this.state} />
+//           <Section title={'Statistics'} />
+//           <Statistics
+//             stats={this.state}
+//             total={this.countTotalFeedback()}
+//             percentage={this.countPositiveFeedbackPercentage()}
+//           />
+//         </Container>
+//       </>
+//     );
+//   }
+// }
